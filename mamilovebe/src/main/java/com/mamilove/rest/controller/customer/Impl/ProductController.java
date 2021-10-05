7eb9f141f9df452c44775504f3a50146e3180c1d
manh pham <pmanh2123@gmail.com>
2021-10-05 08:52:30 +0700

    t commit controller

--- mamilovebe/src/main/java/com/mamilove/rest/controller/customer/Impl/ProductController.java
+++ mamilovebe/src/main/java/com/mamilove/rest/controller/customer/Impl/ProductController.java
@@ -4,13 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mamilove.entity.Product;
+import com.mamilove.request.dto.JwtResponse;
+import com.mamilove.request.dto.Res;
 import com.mamilove.service.service.ImageService;
 import com.mamilove.service.service.ProductService;
 
@@ -23,12 +27,18 @@ public class ProductController {
 	ImageService ImageService;
 	
 	@GetMapping("/list-product")
-	public List<Product> findAll(){
-		return productService.findAll();
+	public ResponseEntity<?> findAll(){
+		List<Product> entity = productService.findAll();
+		return ResponseEntity.ok(new Res( entity , "Success", true));
 	}
 	@GetMapping("/home")
-	public List<Product> findProductNew(){
-		return productService.findProductNew();
+	public ResponseEntity<?> findProductNew(){
+		List<Product> entity = productService.findProductNew();
+		return ResponseEntity.ok(new Res( entity , "Success", true));
 	}
-	
+	@GetMapping("/list-product/info/{id}")
+	public ResponseEntity<?> findProductById(@PathVariable("id") Long id){
+		Optional<Product> entity = productService.findById(id);
+		return ResponseEntity.ok(new Res( entity , "Success", true));
+	}
 }
