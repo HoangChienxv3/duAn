92afc2b2558bbbee9d948c339a42739eb08486b2
Hoang chien <hoangchinv3@gmail.com>
2021-11-12 13:55:09 +0700

    up code Api Event

--- mamilovebe/src/main/java/com/mamilove/dao/EventDao.java
+++ mamilovebe/src/main/java/com/mamilove/dao/EventDao.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mamilove.entity.Event;
 
-public interface EventDao extends JpaRepository<Event, Long>{
+import java.util.List;
+import java.util.Optional;
 
+public interface EventDao extends JpaRepository<Event, Long>{
+    List<Event> findAllByIsDeleteFalse();
+    Optional<Event> findByIdAndIsDeleteFalse(Long id);
 }
---
+++ mamilovebe/src/main/java/com/mamilove/rest/controller/admin/EventManagerController.java
@@ -0,0 +1,44 @@
+package com.mamilove.rest.controller.admin;
+
+import com.mamilove.dao.EventDao;
+import com.mamilove.request.dto.EventRequest;
+import com.mamilove.request.dto.Res;
+import com.mamilove.service.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("/Manager/BillManagerController")
+public class EventManagerController {
+
+    @Autowired
+    EventService eventService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Res> cteateEvent(@RequestBody EventRequest eventRequest) throws ParseException {
+        return ResponseEntity.ok(new Res(eventService.create(eventRequest),"Thêm thành công", true));
+    }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<Res> updateEvent(@PathVariable("id")Long id, @RequestBody EventRequest eventRequest) throws ParseException {
+        return ResponseEntity.ok(new Res(eventService.update(id,eventRequest),"Thêm thành công", true));
+    }
+
+    @GetMapping("/detele/{id}")
+    public ResponseEntity<Res> deteleEvent(@PathVariable("id")Long id){
+        return ResponseEntity.ok(new Res(eventService.detele(id),"Xóa thành công", true));
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<Res> findAll(){
+        return ResponseEntity.ok(new Res(eventService.findAll(),"thành công", true));
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Res> findById(@PathVariable("id")Long id){
+        return ResponseEntity.ok(new Res(eventService.findById(id),"thành công", true));
+    }
+}
