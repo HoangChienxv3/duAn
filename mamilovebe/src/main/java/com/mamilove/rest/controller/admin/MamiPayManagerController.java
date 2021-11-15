827adb4c1b85b915570d556c5fcfb018364cafaa
Hoang chien <hoangchinv3@gmail.com>
2021-11-15 14:38:21 +0700

    updateApi mamipay

--- mamilovebe/src/main/java/com/mamilove/rest/controller/admin/MamiPayManagerController.java
+++ mamilovebe/src/main/java/com/mamilove/rest/controller/admin/MamiPayManagerController.java
@@ -1,17 +1,12 @@
 package com.mamilove.rest.controller.admin;
 
 import com.mamilove.dao.MamiPayDao;
-import com.mamilove.entity.Orderdetail;
 import com.mamilove.request.dto.Res;
-import com.mamilove.vnpay.MamiPayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/Manager/OrderDetailManagerController")
