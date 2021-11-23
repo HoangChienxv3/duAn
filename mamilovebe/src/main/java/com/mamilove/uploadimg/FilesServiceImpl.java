ce2ad0614d20352c5c601a38060e4cde1c27288e
duc-gif <ducmc012@gmail.com>
2021-11-24 02:20:56 +0700

    ducupimg

---
+++ mamilovebe/src/main/java/com/mamilove/uploadimg/FilesServiceImpl.java
@@ -0,0 +1,68 @@
+package com.mamilove.uploadimg;
+
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+@Service
+public class FilesServiceImpl implements FilesSerivce {
+
+    private final Path root = Paths.get("severImg");
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectory(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Không thể khởi tạo thư mục để tải ảnh lên!");
+        }
+    }
+
+    @Override
+    public void save(MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+        } catch (Exception e) {
+            throw new RuntimeException("Không thể lưu trữ tệp. Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Resource load(String filename) {
+        try {
+            Path file = root.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Không thể đọc tệp!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(root.toFile());
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.root, 1).filter(path -> !path.equals(this.root)).map(this.root::relativize);
+        } catch (IOException e) {
+            throw new RuntimeException("Không thể tải các tệp!");
+        }
+    }}
---
+++ mamilovebe/src/main/java/com/mamilove/uploadimg/ResponeMess.java
@@ -0,0 +1,13 @@
+package com.mamilove.uploadimg;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponeMess {
+    private String message;
+
+}
